8efe4b6cad1c872640bc22c8e1f922b148f62263
LexX480 <voidbaka47@gmail.com>
2025-08-02 15:33:02 +0545

    final

---
+++ src/store/useThemeStore.js
@@ -0,0 +1,13 @@
+// store/useThemeStore.js
+import { create } from "zustand";
+
+const useThemeStore = create((set) => ({
+  theme: localStorage.getItem("chat-theme") || "coffee",
+  setTheme: (theme) => {
+    localStorage.setItem("chat-theme", theme);
+    document.documentElement.setAttribute("data-theme", theme);
+    set({ theme });
+  },
+}));
+
+export default useThemeStore;
